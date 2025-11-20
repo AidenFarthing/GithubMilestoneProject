9f1aa8f941989b684703fa7136326a7a0555e631
Aiden Farthing <aiden.farthing@hotmail.co.uk>
2025-11-20 15:58:33 +0000

    project structure

--- src/test/java/com/sparta/utils/Config.java
+++ src/test/java/com/sparta/utils/Config.java
@@ -25,4 +25,7 @@ public class Config {
     public static String getGitHubBaseUri(){
         return bundle.getString("graphql.url");
     }
+    public static String getRESTBaseUri(){
+        return bundle.getString("rest.url");
+    }
 }
